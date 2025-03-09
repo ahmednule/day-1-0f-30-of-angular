b8430b20d8427b8d573a913c9057b80ec4b6b920
ahmednule <nuleahmed6@gmail.com>
2025-03-09 17:42:55 +0300

    feature: imported the home componenet of the application inside the main app page then added the dependacies used to make the application work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
@@ -8,9 +9,13 @@ import { Component } from '@angular/core';
     <header>
       <img src="/assets/logo.svg" alt="This is the logo of the application"/>
       </header>
+      <section>
+        <app-home></app-home>
+      </section>
   </main>
   `,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
 })
 export class AppComponent {
   title = 'homes';
