377b148e8a721d503d8c3abb319d5e15ff0e7a96
ahmednule <nuleahmed6@gmail.com>
2025-03-10 22:03:19 +0300

    feaure(reusableComponenet): created the reusable component which will be used to nest the houses which will be shown in the home section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
 <section class="first-section">
   <form>
@@ -13,9 +14,10 @@ import { CommonModule } from '@angular/common';
   </form>
 </section>
 <section class="second-section">
+  <app-housing-location></app-housing-location>
 </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css',]
 })
 export class HomeComponent {
 
